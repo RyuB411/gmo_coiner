dcf3c0675b99f143cc602d6fcc33264e4772c699
RyuB <ryubline411@gmail.com>
2021-05-07 15:45:24 +0900

    GMOCOINER-0009 ローソク足にSMAライン追加

--- backend/resources/js/components/CandlestickChart.vue
+++ backend/resources/js/components/CandlestickChart.vue
@@ -1,7 +1,7 @@
 <!-- https://github.com/ehajri/vue-chartjs-financial/blob/master/src/views/Candlestick.vue -->
 <template>
   <div class="container border">
-    <Candlestick :chart-data="datacollection" :options="options" />
+    <Candlestick :chart-data="datacollection" :options="options"/>
   </div>
 </template>
 
@@ -31,7 +31,40 @@ export default {
           {
             type: 'candlestick',
             label: 'OHLC',
-            data: ret.data,
+            data: ret.data['ohlc'],
+          },
+          {
+            type: 'line',
+            label: 'sma:7',
+            data: ret.data['sma']['7'],
+            borderColor : "rgba(100,200,250,0.8)",
+            pointStyle: 'line',
+            fill: false,
+            tension: 0,
+          },
+          {
+            type: 'line',
+            label: 'sma:20',
+            data: ret.data['sma']['20'],
+            borderColor : "rgba(50,150,250,0.8)",
+            pointStyle: 'line',
+            fill: false,
+            tension: 0,
+          },
+          {
+            type: 'line',
+            label: 'sma:60',
+            data: ret.data['sma']['60'],
+            borderColor : "rgba(25,100,250,0.8)",
+            pointStyle: 'line',
+            fill: false,
+            tension: 0,
+          },
+          {
+            type: 'bar',
+            label: 'volume',
+            data: ret.data['volume'],
+            borderColor : "rgba(100,100,100,0.5)",
           },
         ],
       };
